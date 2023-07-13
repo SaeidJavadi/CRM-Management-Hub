2dc9550a889438fb4ffa4c46914996095a324d22
Hiding <s.a.e.i.d@live.com>
2023-07-13 12:05:32 +0330

    up wk 5shnb

--- crm/views.py
+++ crm/views.py
@@ -1,75 +1,95 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.views import generic
 from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView, View, DeleteView
-from crm.models import Common60
+from bootstrap_modal_forms.generic import (
+    BSModalLoginView,
+    BSModalFormView,
+    BSModalCreateView,
+    BSModalUpdateView,
+    BSModalReadView,
+    BSModalDeleteView
+)
+from crm.forms import (
+    ObjModelForm,
+    ObjFilterForm
+)
+from crm.models import Book
+
 
 
 @login_required
 def home(request):
     return render(request, 'crm/home.html')
 
-
 @login_required
 def dashboard(request):
     return render(request, 'crm/dashboard.html')
 
 
-class C60sListView(TemplateView):
-    template_name = "crm/c60_list.html"
-    context_object_name = "commons"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['users'] = Common60.objects.all()
-        return context
+class C60List(generic.ListView):
+    model = Book
+    context_object_name = 'objs'
+    template_name = 'crm/c60_list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if 'type' in self.request.GET:
+            qs = qs.filter(book_type=int(self.request.GET['type']))
+        return qs
 
 
-class CreateCrudUser(View):
-    def get(self, request):
-        name1 = request.GET.get('name', None)
-        address1 = request.GET.get('address', None)
-        age1 = request.GET.get('age', None)
+class ObjFilterView(BSModalFormView):
+    template_name = 'curd/filter_obj.html'
+    form_class = ObjFilterForm
 
-        obj = Common60.objects.create(
-            name=name1,
-            address=address1,
-            age=age1
+    def form_valid(self, form):
+        self.filter = '?type=' + form.cleaned_data['type']
+        response = super().form_valid(form)
+        return response
+
+    def get_success_url(self):
+        return reverse_lazy('crm:c60list') + self.filter
+
+
+class ObjCreateView(BSModalCreateView):
+    template_name = 'curd/create_obj.html'
+    form_class = ObjModelForm
+    success_message = 'Success: Obj was created.'
+    success_url = reverse_lazy('crm:c60list')
+
+
+class ObjUpdateView(BSModalUpdateView):
+    model = Book
+    template_name = 'curd/update_obj.html'
+    form_class = ObjModelForm
+    success_message = 'Success: Obj was updated.'
+    success_url = reverse_lazy('crm:c60list')
+
+
+class ObjReadView(BSModalReadView):
+    model = Book
+    template_name = 'curd/read_obj.html'
+
+
+class ObjDeleteView(BSModalDeleteView):
+    model = Book
+    template_name = 'curd/delete_obj.html'
+    success_message = 'Success: Obj was deleted.'
+    success_url = reverse_lazy('crm:c60list')
+
+
+@login_required()
+def objs(request):
+    data = {}
+    if request.method == 'GET':
+        objs = Book.objects.all()
+        data['table'] = render_to_string(
+            '_objs_table.html',
+            {'objs': objs},
+            request=request
         )
-
-        user = {'id': obj.id, 'name': obj.name, 'address': obj.address, 'age': obj.age}
-
-        data = {
-            'user': user
-        }
-        return JsonResponse(data)
-
-
-class DeleteCrudUser(View):
-    def get(self, request):
-        id1 = request.GET.get('id', None)
-        Common60.objects.get(id=id1).delete()
-        data = {
-            'deleted': True
-        }
-        return JsonResponse(data)
-
-
-class UpdateCrudUser(View):
-    def get(self, request):
-        id1 = request.GET.get('id', None)
-        name1 = request.GET.get('name', None)
-        address1 = request.GET.get('address', None)
-        age1 = request.GET.get('age', None)
-
-        obj = Common60.objects.get(id=id1)
-        obj.name = name1
-        obj.address = address1
-        obj.age = age1
-        obj.save()
-
-        user = {'id': obj.id, 'name': obj.name, 'address': obj.address, 'age': obj.age}
-
-        data = {
-            'user': user
-        }
         return JsonResponse(data)
