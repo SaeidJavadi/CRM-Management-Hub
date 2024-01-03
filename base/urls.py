ce0ad69e551166e4e73bc15286a8880fe9056856
Saeid Javadi <S.Javadi.P@gmail.com>
2024-01-03 22:01:17 +0330

    add language sqitch btn

--- base/urls.py
+++ base/urls.py
@@ -8,7 +8,10 @@ from django.conf.urls.i18n import i18n_patterns
 # from rest_framework import permissions
 
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
+    path("i18n/", include("django.conf.urls.i18n")),
+]
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('', include('crm.urls', namespace='crm')),
