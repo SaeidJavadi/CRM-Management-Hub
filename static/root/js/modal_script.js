fe1483962eeeedee72741ae2d3e7c67039ab32d6
Saeid Javadi <s.javadi.p@gmail.com>
2024-01-06 17:47:48 +0330

    np

--- static/root/js/modal_script.js
+++ static/root/js/modal_script.js
@@ -12,15 +12,3 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   modal.close();
 });
-
-// Print Function 
-function printdiv(elem) {
-  var header_str = '<html><head><title>' + document.title  + '</title></head><body>';
-  var footer_str = '</body></html>';
-  var new_str = document.getElementById(elem).innerHTML;
-  var old_str = document.body.innerHTML;
-  document.body.innerHTML = header_str + new_str + footer_str;
-  window.print();
-  document.body.innerHTML = old_str;
-  return false;
-}
