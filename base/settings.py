71b4d96106ab17b252d7f19fa81b96b2173068a7
admin <admin@site.novalocal>
2024-02-05 14:16:14 +0000

    start deploy

--- base/settings.py
+++ base/settings.py
@@ -94,13 +94,23 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "crmdb",
+        'USER': "crmAdmin",
+        'PASSWORD': "crmar94sjp2024",
+        'HOST': "localhost",
+        'PORT': 5430,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -124,8 +134,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'ar'
+LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'ar'
 
 TIME_ZONE = 'UTC'
 
