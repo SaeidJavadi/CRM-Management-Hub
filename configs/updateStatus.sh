946faea7002c8f032514300a903c6cc8b77e9dce
Saeid Javadi <S.Javadi.P@gmail.com>
2024-04-04 09:17:30 +0000

    update cronab status

--- configs/updateStatus.sh
+++ configs/updateStatus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /home/saeid/pyENV/bin/activate
-python /home/saeid/projown/crm_ar/manage.py update_commons_status_1_year > /home/saeid/projown/crm_ar/zTMP/logs/updatestatus.log 2>&1
+source /home/admin/pyENV/bin/activate
+python /home/admin/crmproj/base/manage.py update_commons_status_1_year > /home/admin/crmproj/base/zTMP/logs/updatestatus.log 2>&1
 
 # CronJob
 # At 00:00 on day-of-month 1 in January
